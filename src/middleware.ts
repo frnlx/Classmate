a9438a0cd078e984c5d7633cb32865c07aa1aaa3
Alfonsus Ardani <alfonsusac@gmail.com>
2023-05-30 22:17:36 +0700

    COmmented verbose logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,16 +7,16 @@ export function middleware(request: NextRequest) {
 
   // Logger
   request.headers.get('sec-fetch-dest') === 'document' ? verboselog('\n\n------------------Browser Request') : null
-  request.headers.get('sec-fetch-dest') === 'empty' && !request.url.endsWith('webpack.hot-update.json') ? verboselog('\n\n------------------Browser Hot Update') : null
-  if(request.headers.get('sec-fetch-dest') === 'manifest') verboselog('- fetching manifest data...')
-  else if (request.headers.get('sec-fetch-dest') === 'image') verboselog(`- fetching image: ${request.nextUrl.pathname}`)
-  else if (request.headers.get('sec-fetch-dest') === 'font') verboselog(`- fetching bundled fonts`)
-  else if (request.headers.get('sec-fetch-dest') === 'script') verboselog(`- fetching bundled script (JS)`)
-  else if (request.headers.get('sec-fetch-dest') === 'style') verboselog(`- fetching bundled style (CSS)`)
-  else if (request.headers.get('sec-fetch-dest') === 'document') verboselog(`- fetching document`)
-  else if (request.headers.get('sec-fetch-dest') === 'empty') verboselog(`- refetching document (hot update)`)
-  else if (request.url.endsWith('webpack.hot-update.json')) verboselog(`- refetching document (hot update) with web pack json`)
-  else verboselog(request) 
+  // request.headers.get('sec-fetch-dest') === 'empty' && !request.url.endsWith('webpack.hot-update.json') ? verboselog('\n\n------------------Browser Hot Update') : null
+  // if(request.headers.get('sec-fetch-dest') === 'manifest') verboselog('- fetching manifest data...')
+  // else if (request.headers.get('sec-fetch-dest') === 'image') verboselog(`- fetching image: ${request.nextUrl.pathname}`)
+  // else if (request.headers.get('sec-fetch-dest') === 'font') verboselog(`- fetching bundled fonts`)
+  // else if (request.headers.get('sec-fetch-dest') === 'script') verboselog(`- fetching bundled script (JS)`)
+  // else if (request.headers.get('sec-fetch-dest') === 'style') verboselog(`- fetching bundled style (CSS)`)
+  // else if (request.headers.get('sec-fetch-dest') === 'document') verboselog(`- fetching document`)
+  // else if (request.headers.get('sec-fetch-dest') === 'empty') verboselog(`- refetching document (hot update)`)
+  // else if (request.url.endsWith('webpack.hot-update.json')) verboselog(`- refetching document (hot update) with web pack json`)
+  // else verboselog(request) 
 
   // Is a route not a file request
   if (!request.url.includes('.')) {
