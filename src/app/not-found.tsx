6b92eca719a9de63439cff1d3b28fa246a0e07c7
Alfonsus Ardani <alfonsusac@gmail.com>
2023-06-14 17:20:12 +0700

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,10 @@
+import AppBackground from "@/components/static/background"
+
 export default function NotFound() {
   return (
-    <>
-      <h2>Not Found from the Root Layout</h2>
+    <AppBackground>
+      <h2>Classroom Not Found</h2>
       <p>Could not find requested resource</p>
-    </>
+    </AppBackground>
   );
 }
