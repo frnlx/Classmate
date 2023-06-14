62caead4f08f86728268140f765d67d05b888efe
Alfonsus Ardani <alfonsusac@gmail.com>
2023-06-14 17:24:21 +0700

    Udpated ClassroomRoutePage

--- src/app/(member)/[classid]/page.tsx
+++ src/app/(member)/[classid]/page.tsx
@@ -3,7 +3,7 @@ import { color } from "@/lib/logger/chalk"
 import { redirect } from "next/navigation"
 
 export default async function ClassroomTempPage(p: { params: any }) {
-  redirect(`/${p.params.classid}/home`)
+  return redirect(`/${p.params.classid}/home`)
   // return <>Test</>
 }
 // https://nextjs.org/docs/app/api-reference/file-conventions/page
