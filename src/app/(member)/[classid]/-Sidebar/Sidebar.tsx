fba0944707f9b2bf3819ce6657acd391b90cce2d
Alfonsus Ardani <alfonsusac@gmail.com>
2023-06-14 17:22:29 +0700

    Separated server and client rendered sidebar
    to reduce flicker

--- src/app/(member)/[classid]/-Sidebar/Sidebar.tsx
+++ src/app/(member)/[classid]/-Sidebar/Sidebar.tsx
@@ -1,139 +1,33 @@
-'use client'
-import { useClassroomQuery } from "@/api/client/classroom";
-import { ReactNode, useEffect, useTransition } from "react";
-import { List } from "@radix-ui/react-tabs";
+import { ReactNode, Suspense } from "react";
 import { useRoom } from "../../-Navbar/Navbar";
-import { useCreateCategory } from "@/api/client/category";
-import { SidebarItem } from "./SidebarItem";
-import { SidebarCategoryIcon } from "./SidebarIcons";
-import { CaretDown, Plus } from "@phosphor-icons/react";
 import { Category, Classroom } from "@prisma/client"
-import clsx from "clsx"
-import { ButtonTooltip } from "@/components/use-client/Tooltip"
-import { addCategory } from "./addsidebar"
-import { useRouter } from "next/navigation"
-import { useQuery, useQueryClient } from "@tanstack/react-query"
-import { ClientAPI } from "@/api/client/api"
 import { useUserid } from "@/api/client/auth"
-
+import { List, SidebarHeader, CategoryList, AddCategoryButton } from "./SidebarClient"
+import { getClassroomData, getUserData } from "../../utils"
+import { notFound } from "next/navigation"
+import { getCachedSession, getUserId } from "@/lib/auth-helper"
 
 export default function Sidebar(p: {
   children: ReactNode
-  prefetchedclasslist: Category[]
-  prefetchedclassdata: Classroom
 }) {
-
-  const { currentId } = useRoom()
-  const userid = useUserid()
-
-  // const { data: classroom } = useClassroomQuery(currentId, p.prefetchedclassdata)
-  // const { data: categoryList } = useClassCategories(currentId, p.prefetchedclasslist)
-
-  const { data: categoryList } = useQuery({
-    queryKey: ['classrooms', currentId, 'categorylist'],
-    queryFn() {
-      return ClientAPI.getCategoryList({ userid, classid: currentId })
-    },
-    enabled: !!userid && !!currentId,
-    initialData: p.prefetchedclasslist,
-    staleTime: 30,
-    refetchOnWindowFocus: false,
-  })
+  
 
   return (
     <List className='bg-dark1 w-56 h-screen flex-shrink-0 gap-2' loop>
 
-      <SidebarHeader currentId={ currentId } />
+      <SidebarHeader />
       <div className="py-2 px-2">
         {p.children}
       </div>
       <div className="py-2 px-2">
-        {/* <CategoryList currentId={ currentId } categoryList={ categoryList! } /> */}
-        <CategoryList currentId={ currentId } categoryList={ categoryList } />
+        <div className="font-bold text-xs text-light2 px-2 py-0.5 flex justify-between">
+          Categories
+          <AddCategoryButton />
+        </div>
+        <CategoryList />
       </div>
       
     </List>
   )
   
-}
-
-
-function SidebarHeader(p: {
-  currentId: string
-}) {
-
-  const { data } = useClassroomQuery( p.currentId )
-  return (
-    <div className={ clsx(
-      "font-bold leading-5 py-4 px-4 border-slate-700 truncate",
-      "text-sm",
-      "hover:bg-dark2/50 group",
-      "flex",
-      "gap-1"
-    ) }>
-      <div className="truncate text-white">
-        { data ? data.name : 'loading...' }
-      </div>
-      <CaretDown size={16} weight={"bold"} className="fill-light1 group-hover:fill-light0"/>
-    </div>
-  )
-      
-}
-
-
-
-function CategoryList(p: {
-  currentId: string
-  categoryList: Category[]
-}) {
-  useEffect(() => {
-    console.log(`CategoryList Rerender ${p.currentId}`)
-  },[])
-  return (
-    <>
-      <div className="font-bold text-xs text-light2 px-2 py-0.5 flex justify-between">
-        Categories
-        <AddCategoryButton />
-      </div>
-      {
-        p.categoryList ? p.categoryList.map((page, idx) =>
-          <SidebarItem
-            key={page.id}
-            id={page.id}
-            icon={<SidebarCategoryIcon />}
-            label={page.id}
-            isCategory // enables context menu
-          />) : <>Loading Categories</>
-      }
-    </>
-  )
-}
-
-
-function AddCategoryButton() {
-
-  const room = useRoom()
-  const createCategory = useCreateCategory(room.currentId ?? '')
-
-  const [isPending, startTransition] = useTransition()
-  const router = useRouter()
-
-  const qc = useQueryClient()
-
-  return (
-    <ButtonTooltip label="Add New Category">
-      <button
-        className="text-light2 hover:text-light0 text-sm"
-        onClick={ () => startTransition(() => {
-          return addCategory(room.currentId).then(() => {
-            qc.invalidateQueries(['classrooms', room.currentId, 'categorylist'])
-            // router.refresh()
-          })
-        }) }
-      >
-        <Plus weight="bold" />
-      </button>
-    </ButtonTooltip>
-  )
-
 }
---
+++ src/app/(member)/[classid]/-Sidebar/SidebarClient.tsx
@@ -0,0 +1,123 @@
+'use client'
+
+import { ClientAPI } from '@/api/client/api'
+import { useUserid } from '@/api/client/auth'
+import { useClassroomQuery } from '@/api/client/classroom'
+import { CaretDown, Plus } from '@phosphor-icons/react'
+import { Category, Classroom, User } from '@prisma/client'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import clsx from 'clsx'
+import { useEffect, useTransition } from 'react'
+import { SidebarItem } from './SidebarItem'
+import { SidebarCategoryIcon } from './SidebarIcons'
+import { useRoom } from '../../-Navbar/Navbar'
+import { useCreateCategory } from '@/api/client/category'
+import { useRouter } from 'next/navigation'
+import { addCategory } from './addsidebar'
+import { ButtonTooltip } from '@/components/use-client/Tooltip'
+
+export { List } from '@radix-ui/react-tabs'
+
+
+export function SidebarHeader(p: {
+}) {
+  const { currentId, userData } = useRoom()
+  // const { data } = useClassroomQuery(currentId)
+  const userid = useUserid()
+  const qc = useQueryClient()
+  
+  const { data } = useQuery({
+    queryKey: ['classrooms', currentId, 'data'],
+    queryFn() {
+      return ClientAPI.getClassroom({ userid, classid: currentId })
+    },
+    enabled: !!userid && !!currentId,
+    initialData: userData.classes.find(c => c.id === currentId),
+  })
+
+  return (
+    <div className={ clsx(
+      "font-bold leading-5 py-4 px-4 border-slate-700 truncate",
+      "text-sm",
+      "hover:bg-dark2/50 group",
+      "flex",
+      "gap-1"
+    ) }>
+      <div className="truncate text-white">
+        { data ? data.name : 'loading...' }
+      </div>
+      <CaretDown size={ 16 } weight={ "bold" } className="fill-light1 group-hover:fill-light0" />
+    </div>
+  )
+
+}
+
+export function CategoryList(p: {
+}) {
+  const { currentId, userData } = useRoom()
+  const userid = useUserid()
+
+  const qc = useQueryClient()
+
+  // So ugly 🤮
+  const { data: categoryList, error } = useQuery({
+    queryKey: ['classrooms', currentId, 'categorylist'],
+    queryFn() {
+      return ClientAPI.getCategoryList({ userid, classid: currentId })
+    },
+    enabled: !!userid && !!currentId,
+
+    initialData: userData.classes.find(c => c.id === currentId)?.categories,
+
+    staleTime: 30,
+    refetchOnWindowFocus: false,
+  })
+
+  useEffect(() => {
+    console.log(`CategoryList Rerender ${currentId}`)
+  }, [])
+  return (
+    <>
+      {
+        categoryList ? categoryList.map((page, idx) =>
+          <SidebarItem
+            key={ page.id }
+            id={ page.id }
+            icon={ <SidebarCategoryIcon /> }
+            label={ page.id }
+            isCategory // enables context menu
+          />) : <>Loading Categories</>
+      }
+    </>
+  )
+}
+
+
+
+export function AddCategoryButton() {
+
+  const room = useRoom()
+  const createCategory = useCreateCategory(room.currentId ?? '')
+
+  const [isPending, startTransition] = useTransition()
+  const router = useRouter()
+
+  const qc = useQueryClient()
+
+  return (
+    <ButtonTooltip label="Add New Category">
+      <button
+        className="text-light2 hover:text-light0 text-sm"
+        onClick={ () => startTransition(() => {
+          return addCategory(room.currentId).then(() => {
+            qc.invalidateQueries(['classrooms', room.currentId, 'categorylist'])
+            // router.refresh()
+          })
+        }) }
+      >
+        <Plus weight="bold" />
+      </button>
+    </ButtonTooltip>
+  )
+
+}
