032647331a3d118db889fb59f0f667b677d77ed2
frnlx <eugenereginaldp@gmail.com>
2023-06-16 18:56:25 +0700

    Add create resources API endpoint

--- src/api/route/resource.ts
+++ src/api/route/resource.ts
@@ -2,11 +2,13 @@ import { HandlerLookup, RouteLookupType } from "@/lib/route";
 import { membersOnly } from "../utils";
 import { prisma } from "@/lib/db";
 import { nanoid } from "nanoid";
+import { ResourceFormSchema } from "@/components/classroom/category/resources/AddResource";
+import { ResourceType } from "@prisma/client";
 
 
 const resource = {
 
-  async getData(_, res, [uid, cid, catid, sid, rid]) {
+  async getData(_, res, [uid, cid, catid, rid]) {
     await membersOnly()
 
     const data = await prisma.user.findUniqueOrThrow({
@@ -18,14 +20,9 @@ const resource = {
             categories: {
               where: { id: catid },
               include: {
-                sections: {
-                  where: { id: sid },
-                  select: {
-                    post: {
-                      where: {
-                        id: rid,
-                      }
-                    }
+                Resource: {
+                  where: {
+                    id: rid,
                   }
                 }
               }
@@ -35,16 +32,57 @@ const resource = {
       }
     })
 
-    return res.json(data.classes[0].categories[0].sections[0].post[0])
+    return res.json(data.classes[0].categories[0].Resource[0])
   },
 
+  async createResource(_, res, [uid, cid, catid, rid], body: ResourceFormSchema) {
+    const resource = await prisma.resource.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        category: {
+          connect: { id: catid }
+        },
+        type: body.type,
+        order: 0,
+      }
+    })
 
+    if (body.type === ResourceType.NORMAL_POST) {
+      await prisma.normalPost.create({
+        data: {
+          resource: { connect: {id: resource.id} }
+        }
+      })
+    } else if (body.type === ResourceType.ASSIGNMENT) {
+      await prisma.assignment.create({
+        data: {
+          dueDate: body.dueDate,
+          point: body.point,
+          xpReward: body.xp,
+          resource: { connect: {id: resource.id} }
+        }
+      })
+    } else {
+      await prisma.discussion.create({
+        data: {
+          dueDate: body.dueDate,
+          point: body.point,
+          xpReward: body.xp,
+          resource: { connect: {id: resource.id} }
+        }
+      })
+    }
+
+    return res.json(resource)
+  }
 
 } satisfies HandlerLookup
 
 export const resourceRoutes: RouteLookupType = {
 
   // Get Resource	https://notion.so/skripsiadekelas/b362f54d439f48e2ba91828fb82c4590
-  'GET:/classrooms/[classid]/categories/[categoryid]/sections/[sectionid]/resources/[resourceid]': resource.getData
+   'GET:/users/[userid]/classrooms/[classid]/categories/[categoryid]/resources/[resourceid]': resource.getData,
+  'POST:/users/[userid]/classrooms/[classid]/categories/[categoryid]':                        resource.createResource
   
 } 
