b381c74e96765958c0763919620486dd05c4f076
frnlx <eugenereginaldp@gmail.com>
2023-06-17 20:16:32 +0700

    Fix class creation

--- src/components/form/createClass.ts
+++ src/components/form/createClass.ts
@@ -1,31 +1,30 @@
-'use server'
+"use server";
 
-import { InferedCreateClassroomFormSchema } from "./CreateClassForm"
-import { AuthOptions, CallbacksOptions, Session } from "next-auth"
-import { color } from "@/lib/logger/chalk"
-import { authOptions } from "@/configs/auth"
-import { cookies, headers } from "next/headers"
-import { getToken } from "next-auth/jwt"
-import { NextRequest } from "next/server"
-import { AuthHandler } from "next-auth/core"
+import { InferedCreateClassroomFormSchema } from "./CreateClassForm";
+import { AuthOptions, CallbacksOptions, Session } from "next-auth";
+import { color } from "@/lib/logger/chalk";
+import { authOptions } from "@/configs/auth";
+import { cookies, headers } from "next/headers";
+import { getToken } from "next-auth/jwt";
+import { NextRequest } from "next/server";
+import { AuthHandler } from "next-auth/core";
 // @ts-ignore
-import { serialize } from "cookie"
-import { Cookie } from "next-auth/core/lib/cookie"
-import { getServerActionSession } from "@/lib/auth"
-import { prisma } from "@/lib/db"
-import { redirect } from "next/navigation"
-import { nanoid } from "nanoid"
-import { revalidatePath } from "next/cache"
+import { serialize } from "cookie";
+import { Cookie } from "next-auth/core/lib/cookie";
+import { getServerActionSession } from "@/lib/auth";
+import { prisma } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { nanoid } from "nanoid";
+import { revalidatePath } from "next/cache";
 
 export async function SAcreateClass(values: InferedCreateClassroomFormSchema) {
+  color.yellow("SA Create Class");
 
-  color.yellow('SA Create Class')
+  const session = await getServerActionSession(authOptions);
 
-  const session = await getServerActionSession(authOptions)
+  if (!session) redirect("/auth");
 
-  if(!session) redirect('/auth')
-
-  console.log(session)
+  console.log(session);
 
   const classroom = await prisma.classroom.create({
     data: {
@@ -33,35 +32,22 @@ export async function SAcreateClass(values: InferedCreateClassroomFormSchema) {
       subject: values.subject,
       emoji: values.emoji,
       members: {
-        connect: { id: session.user.id } // will throw if not found
+        connect: { id: session.user.id }, // will throw if not found
       },
       owner: {
-        connect: { id: session.user.id } // will throw if not found
+        connect: { id: session.user.id }, // will throw if not found
       },
       inviteID: nanoid(6),
       categories: {
         create: [
           {
-            name: 'Overview',
-            title: 'My First Topic',
-            sections: {
-              create: [
-                {
-                  name: 'Introduction',
-                  order: 0
-                }
-              ]
-            }
-          }
-        ]
-      }
-    }
-  })
+            title: "My First Topic",
+          },
+        ],
+      },
+    },
+  });
   // revalidatePath('/')
-  color.yellow('SA Create End')
-  return classroom
-
-
-
-
+  color.yellow("SA Create End");
+  return classroom;
 }
